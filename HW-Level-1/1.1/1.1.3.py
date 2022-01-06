134bca3624e8ca4a8b3d03fae0934dfe56c73d66
cdly189 <83979942+cdly189@users.noreply.github.com>
2022-01-06 14:00:01 -0500

    Add files via upload

---
+++ HW-Level-1/1.1/1.1.3.py
@@ -0,0 +1,11 @@
+"""
+Each word is seperated with a tab
+"""
+
+
+def main():
+    print('Hello \tWorld!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ HW-Level-1/1.1/1.1.4.py
@@ -0,0 +1,12 @@
+"""
+A program take input from user using input function
+"""
+
+
+def main():
+    user_input = input("What's your favorite color? ")
+    print(user_input)
+
+
+if __name__ == '__main__':
+    main()
