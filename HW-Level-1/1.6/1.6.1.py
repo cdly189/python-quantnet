9a46b89412aa6d53c262e73f73253bd7dd16a711
cdly189 <83979942+cdly189@users.noreply.github.com>
2022-01-06 14:32:26 -0500

    Add files via upload

---
+++ HW-Level-1/1.6/1.6.1.py
@@ -0,0 +1,14 @@
+"""
+Import the math module and demonstrate some built-in functions
+"""
+import math
+
+
+def main():
+    print(math.gcd(10, 125))
+    print(math.sqrt(225))
+    print(math.log10(10))
+
+
+if __name__ == '__main__':
+    main()
